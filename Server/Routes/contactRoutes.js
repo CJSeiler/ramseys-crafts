447259430308ca6a58132aedd34d827bcc502373
CJSeiler <82341510+CJSeiler@users.noreply.github.com>
2023-02-07 14:02:29 -0500

    Implementing EmailJS
    Implemented EmailJS functionality including Google reCAPTCHA V2 checkbox functionality.  Set up API endpoint to verify token for reCAPTCHA. Set up functionality to send form info to emailjs

---
+++ Server/Routes/contactRoutes.js
@@ -0,0 +1,32 @@
+import express from "express"
+import asyncHandler from "express-async-handler"
+import axios from "axios"
+
+const contactRouter = express.Router()
+
+// RECAPTCHA TOKEN VERIFICATION POST ROUTE
+contactRouter.post("/verify-token", asyncHandler( async(req, res) => {
+    try {
+        const { token } = req.body
+
+        // sends secrect key and token to google api
+        const response = await axios.post(
+            `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.REACT_APP_SECRET_KEY}&response=${token}`
+        )
+
+        return res.status(200).json({
+            success: true,
+            message: "Token successfully verified",
+            data: response.data
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Error verifying token"
+        })
+    }
+    
+}))
+
+export default contactRouter
--- Server/server.js
+++ Server/server.js
@@ -1,22 +1,27 @@
 import express from "express"
+import cors from "cors"
 import dotenv from "dotenv"
 import connectDatabase from "./config/MongoDb.js"
 import ImportData from "./Dataimport.js"
 import productRouter from "./Routes/ProductRoutes.js"
 import userRouter from "./Routes/UserRoutes.js"
 import orderRouter from "./Routes/orderRoutes.js"
+import contactRouter from "./Routes/contactRoutes.js"
 import { notFound, errorHandler } from "./Middleware/Errors.js"
 
 dotenv.config()
 connectDatabase() 
 const app = express()
+
 app.use(express.json())
+app.use(cors())
 
 // API
 app.use("/api/import", ImportData)
 app.use("/api/products", productRouter)
 app.use("/api/users", userRouter)
 app.use("/api/orders", orderRouter)
+app.use("/api/contact", contactRouter)
 
 // ERROR HANDLER
 app.use(notFound)

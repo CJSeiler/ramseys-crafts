b6d1cb4055499a5a12c6951866ff20a0baeda835
CJSeiler <82341510+CJSeiler@users.noreply.github.com>
2023-06-24 15:27:50 -0400

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://ramseys-crafts-api.onrender.com",
+    origin: "https://ramseys-craft-store.onrender.com",
     methods: "*",
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
